a788e66ee6c7ba1ce83751a6f4f044585437298c
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-01-26 09:17:09 +0545

    update: removed routes to new file

--- backend/server.ts
+++ backend/server.ts
@@ -1,26 +1,14 @@
 const express = require('express');
-
-import bodyParser from 'body-parser';
-
-const app = express();
 const port = 5000;
+
+
 import db from './models/index'
+import { MainRoutes } from './routes/MainRoutes' 
 
-import noteRoute from './routes/noteRoutes'
-import tagRoute from './routes/tagRoutes'
-
-app.use(express.json());
-
-// using body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-app.use('/note', noteRoute);
-app.use('/tag', tagRoute);
+const mainRoutes = new MainRoutes();
 
 db.sequelize.sync().then(() => {
-    app.listen(port, () => {
+    mainRoutes.app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     }); 
 });
